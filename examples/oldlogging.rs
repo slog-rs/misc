1cae20e5d376ff158348f163c93e643680e2e1b1
Robin Stocker <robin@nibor.org>
2017-05-30 10:34:28 +1000

    Fix remaining examples
    And move the rest to a slog1 subdirectory. With this, the project builds
    again without errors.

--- examples/oldlogging.rs
+++ examples/oldlogging.rs
@@ -1,15 +1,25 @@
 #[macro_use]
 extern crate slog;
 extern crate slog_term;
+extern crate slog_scope;
 extern crate slog_stdlog;
 #[macro_use]
 extern crate log;
 
-use slog::DrainExt;
+use std::io;
+
+use slog::Drain;
 
 fn main() {
-    let log = slog::Logger::root(slog_term::streamer().stderr().build().fuse(), o!("version" => "0.5"));
-    slog_stdlog::set_logger(log.clone()).unwrap();
+    let decorator = slog_term::PlainSyncDecorator::new(io::stderr());
+    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let logger = slog::Logger::root(drain, o!("version" => "0.5"));
+
+    // slog_stdlog uses the logger from slog_scope, so set a logger there
+    let _guard = slog_scope::set_global_logger(logger);
+
+    // register slog_stdlog as the log handler with the log crate
+    slog_stdlog::init().unwrap();
 
     info!("standard logging redirected to slog");
 }
